0bf6702bfea5ac711857cdbd6be55c1336d3aa50
BogyunJeong <ototkk58@naver.com>
2025-01-26 00:08:06 +0900

    sum

--- 구간_합_구하기_4.py
+++ 구간_합_구하기_4.py
@@ -1,16 +1,17 @@
 import sys
 
+
 N,M = map(int,sys.stdin.readline().split())
 arr = list(map(int,sys.stdin.readline().split()))
-
-# 전체 합 구하기
-total_sum = 0
-lst = []
+#전체 합 구하기
+cnt = 0
+allsum = [0]
 for i in range(N):
-    total_sum += arr[i]
-    lst.append(total_sum)
+    cnt += arr[i]
+    allsum.append(cnt)
+
 #부분 합 구하기
 for i in range(M):
     a,b = map(int,sys.stdin.readline().split())
-    result = lst[b] - lst[a-1]
+    result = allsum[b] - allsum[a-1]
     print(result)

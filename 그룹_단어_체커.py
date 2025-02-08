105169cba7405eaccf2e8119d5b6e7b74e38ca6b
BogyunJeong <ototkk58@naver.com>
2025-02-09 00:40:51 +0900

    0209

--- 그룹_단어_체커.py
+++ 그룹_단어_체커.py
@@ -1,6 +1,16 @@
 N = int(input())
-for i in range(N):
-    arr = list(map(int,input().split()))
+cnt = N # 정답 카운트
 
-    for i in range(len(arr)):
-        
+
+
+for i in range(N):
+    arr = input()
+
+    for j in range(len(arr)-1):
+        if arr[j] != arr[j+1]:
+            if arr[j] in arr[j+1:]:
+                cnt-=1
+                break
+        elif arr[j] == arr[j+1]:
+            pass
+print(cnt)
